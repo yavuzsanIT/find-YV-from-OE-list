e0b87260dc0194c623059786d7876c7995fcbb5e
ARIKBOGA <you@example.com>
2025-09-01 12:59:27 +0300

    Add Vite configuration for React project setup

---
+++ frontend/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    base: '/find-YV-from-OE-list',
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+
+    },
+});
